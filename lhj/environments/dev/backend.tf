14e04aee0a91f27bdf6bb4a58d03886ab280d70b
LEE-HYEON-JI <1215lhj@gmain.com>
2026-06-10 16:50:47 +0900

    fix: replace use_lockfile with dynamodb_table for terraform 1.7 compatibility

--- lhj/environments/dev/backend.tf
+++ lhj/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "team4-aiops-tfstate-089955620282"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-northeast-2"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "fiveline-tfstate-089955620282"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "fiveline-tflock"
   }
 }
